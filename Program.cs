51b2c1cd89d47c4e33d801e5ac9531f65830f346
Загир Валитов <zagir-valitov@outlook.com>
2024-09-22 14:46:06 +0300

    Добавил обработчик событий DisplayMessage() И подписался

--- Program.cs
+++ Program.cs
@@ -3,9 +3,12 @@
 Console.WriteLine(" ---- Home work 9 ----\n");
 
 var imageDownloader = new ImageDownloader();
+imageDownloader.Notify += DisplayMessage;
 imageDownloader.Download("BigPic_1.jpg", "https://get.wallhere.com/photo/" +
     "landscape-mountains-lake-nature-reflection-grass-sky-river-national-" +
     "park-valley-wilderness-Alps-tree-autumn-leaf-mountain-season-tarn-lo" +
     "ch-mountainous-landforms-mountain-range-590185.jpg");
 Console.WriteLine("Press any key to exit");
 Console.Read();
+
+void DisplayMessage(string message) => Console.WriteLine(message);
