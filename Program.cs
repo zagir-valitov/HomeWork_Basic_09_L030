5ef4689ce72be1d5644288cf0b88d93db0fd824d
Загир Валитов <zagir-valitov@outlook.com>
2024-09-22 11:09:51 +0300

    Перевод строки ссылки на картину

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@
 Console.WriteLine(" ---- Home work 9 ----\n");
 
 var imageDownloader = new ImageDownloader();
-imageDownloader.Download("BigPic_1.jpg", "https://get.wallhere.com/photo/landscape-mountains-lake-nature-reflection-grass-sky-river-national-park-valley-wilderness-Alps-tree-autumn-leaf-mountain-season-tarn-loch-mountainous-landforms-mountain-range-590185.jpg");
+imageDownloader.Download("BigPic_1.jpg", "https://get.wallhere.com/photo/" +
+    "landscape-mountains-lake-nature-reflection-grass-sky-river-national-" +
+    "park-valley-wilderness-Alps-tree-autumn-leaf-mountain-season-tarn-lo" +
+    "ch-mountainous-landforms-mountain-range-590185.jpg");
 Console.WriteLine("Press any key to exit");
 Console.Read();
