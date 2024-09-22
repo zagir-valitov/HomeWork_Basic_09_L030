2c9364c8ad2b7cb7718200056f5f02d2da1e9045
Загир Валитов <zagir-valitov@outlook.com>
2024-09-22 10:26:28 +0300

    Добавил класс ImageLoader

---
+++ ImageDownloader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Basic_09_L030
+{
+    internal class ImageDownloader
+    {
+
+    }
+}
