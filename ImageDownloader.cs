8d7b3f5acfec93eb9280ceeccacf9e885f3186a4
Загир Валитов <zagir-valitov@outlook.com>
2024-09-22 10:38:10 +0300

    Реализовал метод Download в классе ImageDownloader

--- ImageDownloader.cs
+++ ImageDownloader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,12 @@ namespace HomeWork_Basic_09_L030
 {
     internal class ImageDownloader
     {
-
+        public void Download(string remoteUri, string fileName)
+        {
+            var myWebClient = new WebClient();
+            Console.WriteLine($"Downloading a {fileName} from {remoteUri}");
+            myWebClient.DownloadFile( remoteUri, fileName );
+            Console.WriteLine($"Successfully downloaded a {fileName} from {remoteUri}");
+        }
     }
 }
